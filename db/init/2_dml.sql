68dd49df6068e8c08b525b8a9119bcd24e16a496
Mori Keigo <yuzumori.keigo@gmail.com>
2020-10-07 14:17:42 +0900

    DEL docker削除

--- db/init/2_dml.sql
+++
@@ -1,7 +0,0 @@
-use todo_db;
-
-INSERT INTO todo_db.tag(id, tag_data) VALUES( 1, '家事');
-INSERT INTO todo_db.tag(id, tag_data) VALUES( 2, '勉強');
-INSERT INTO todo_db.tag(id, tag_data) VALUES( 3, '就活');
-INSERT INTO todo_db.tag(id, tag_data) VALUES( 4, '課題');
-INSERT INTO todo_db.tag(id, tag_data) VALUES( 5, 'その他');
