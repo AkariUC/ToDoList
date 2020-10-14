f0836e9753aad88e53ef36c69fecd74f5aef5ef6
AkariUC <akari@pluslab.org>
2020-10-14 11:35:26 +0900

    ADD グローバル変数にcompleteを追加しました

--- constant/constant.go
+++ constant/constant.go
@@ -4,5 +4,8 @@ const (
 	// 存在の有無(1 : 存在している , 0 : 存在していない)
 	ExistenceFull = 1
 	ExistenceNull = 0
-	CompleteNot   = 1
+
+	// Todoの終了したかしていないか (1 : 終了していない , 0 : 終了した)
+	CompleteNot = 1
+	Complete    = 0
 )
